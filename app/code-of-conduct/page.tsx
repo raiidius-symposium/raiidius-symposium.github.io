d9f4a5e6ae067e935a85039a09f4fbe3d5f412df
Harry Reyes Nieva <harryreyesnieva@gmail.com>
2026-03-17 16:33:06 -0400

    init

--- app/code-of-conduct/page.tsx
+++ app/code-of-conduct/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Metadata } from 'next'
 import { Container } from '@/components/container'
 
--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // Uncomment and update if deploying to a subpath (e.g., https://username.github.io/repo-name/)
-  basePath: '/raiidius',
-  assetPrefix: '/raiidius/',
+  // basePath: '/repo-name',
+  // assetPrefix: '/repo-name/',
 }
 
 export default nextConfig
